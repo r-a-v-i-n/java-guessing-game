da89cb229f6ef2cb6f089e4fe0d64a05c3691415
Kelsey Ravin <kelsey.ravin@gmail.com>
2021-06-01 20:08:32 +0000

    guessing game lab

--- Game.java
+++ Game.java
@@ -29,6 +29,10 @@ public class Game {
     while (true){
       System.out.print( "Enter your guess: " );
       int guess = input.nextInt();
+     
+        String bad_input = input.next();
+          System.out.println("That's not an integer, try again");
+          continue;
       number_of_guesses += 1;
 
   // In the same way, take another input in a new variable.
@@ -37,7 +41,7 @@ public class Game {
             System.out.println("Your guess is too high, try again.");
         } else if (guess < number){
             System.out.println("Your guess is too low, try again.");
-        } else if (guess != int guess){
+        } else if (guess != ){
             System.out.printIn("Please enter a number.");
         } else if (guess > 100){
             System.out.printIn("It's not greater than 100, dumbass.");
@@ -50,3 +54,85 @@ public class Game {
       }
   }
 }
+
+
+
+
+
+
+
+
+
+
+// solution from coursework, used for learning from how others did this
+
+
+/*
+
+A number-guessing game.
+
+*/
+
+import java.util.Random;
+import java.util.Scanner;  
+import java.util.InputMismatchException;
+
+public class Game {
+  public static void main(String[] args){
+    System.out.println("Howdy, what's your name?");
+    System.out.println("(type in your name) ");
+
+    Scanner input = new Scanner(System.in);
+
+    String name = input.nextLine();
+
+    System.out.println(name + " I'm thinking of a number between 1 and 100.");
+    System.out.println("Can you guess what the number is?");
+
+
+    Random rand = new Random();
+    int number = rand.nextInt(100);
+    int tries = 0;
+
+    System.out.println("The number is "+number);
+
+    System.out.println(name + " I'm thinking of a number between 1 and 100.");
+    System.out.println("Can you guess what the number is?");
+
+    while (true) {
+        int guess;
+        try {
+            guess = input.nextInt();
+        } catch(InputMismatchException e) {
+            String bad_input = input.next();   // need to progress past bad input
+            System.out.println("That's not an integer, try again");
+            continue;
+        }        
+        
+        if (guess < 1 || guess > 100){
+            System.out.println(guess + " is not a valid number, try again");
+            continue;
+        }
+
+        tries = tries + 1;
+
+        if (guess < number) {
+            System.out.println("Your guess is too low, try again.");
+        
+        } else if (guess > number) {
+            System.out.println("Your guess is too high, try again.");    
+        
+        } else {
+            System.out.println("Well done " + name);
+            System.out.println("You found my number in " + tries + " tries!");
+            break;
+        }
+
+    }
+
+  }
+
+}
+
+
+
